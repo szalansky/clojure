283c9d0795ccf7f7038cae20f574f8cc74dfa0e4
Raf Szalanski <rafal.szalanski@alliants.com>
2016-02-19 17:24:33 +0000

    Use clojure.core/str to stringify keywords in clojure.walk/stringify-keys
    Since keywords containing namespaces and a name are valid (i.e. :a/b)
    we should be stringifying both things if a map uses keywords like that
    
    => (clojure.walk/stringify-keys {:a/b 1})
    {"a/b" 1}

--- src/clj/clojure/walk.clj
+++ src/clj/clojure/walk.clj
@@ -102,7 +102,7 @@ the sorting function."}
   "Recursively transforms all map keys from keywords to strings."
   {:added "1.1"}
   [m]
-  (let [f (fn [[k v]] (if (keyword? k) [(name k) v] [k v]))]
+  (let [f (fn [[k v]] (if (keyword? k) [(str k) v] [k v]))]
     ;; only apply to maps
     (postwalk (fn [x] (if (map? x) (into {} (map f x)) x)) m)))
 
